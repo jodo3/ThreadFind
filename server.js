9aff09cbd1b74c005919d421258a47dbff1511ee
jodo3 <johndong3@hotmail.com>
2024-06-09 16:19:55 -0400

    main project

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { searchComments } from './search.js'
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', async(req, res) => {
+  res.render("index");
+})
+
+app.get('/search', async (req, res) => {
+  const { threads, pages } = await searchComments(req.query.video, req.query.user || undefined);
+  res.render("results", {
+    vidurl: req.query.video,
+    user: req.query.user || "OP channel",
+    threads,
+    pages,
+  });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
